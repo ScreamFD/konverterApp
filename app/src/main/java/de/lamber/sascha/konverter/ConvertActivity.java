32bae0a48334c936e8e03828310b29e42e5a2231
Sascha Lamber <sascha.lamber@gmail.com>
2016-01-04 10:26:47 +0100

    initialise git create ConvertLayout

---
+++ app/src/main/java/de/lamber/sascha/konverter/ConvertActivity.java
@@ -0,0 +1,13 @@
+package de.lamber.sascha.konverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConvertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convert);
+    }
+}
